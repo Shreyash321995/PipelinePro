21dc72b1bf8774a5218b978b187be71c18921da0
Shreyash <ubuntu@ip-10-0-14-195.ap-south-1.compute.internal>
2026-04-21 07:07:46 +0000

    app script change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.end('<h1>🚀 CloudPipeline App Running Successfully!</h1>');
+	    res.end('<h1> CloudPipeline App Running Successfully!</h1>');
 });
 
 server.listen(3000, () => {
